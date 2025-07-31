cad93dfc91c3bb32730afdc2038dca0c80030738
Gabriel Mello Silva <gabriel.zyx@gmail.com>
2025-07-30 22:42:37 -0300

    DOCS: Review component docstring

--- streamlit_arborist/__init__.py
+++ streamlit_arborist/__init__.py
@@ -1,47 +1,22 @@
 import os
-from functools import partial
-from typing import Dict, List, Union
+from typing import Callable, Dict, List, Union
 
 import streamlit.components.v1 as components
-from streamlit.runtime.state import WidgetArgs, WidgetCallback, WidgetKwargs
 
-# Create a _RELEASE constant. We'll set this to False while we're developing
-# the component, and True when we're ready to package and distribute it.
-# (This is, of course, optional - there are innumerable ways to manage your
-# release process.)
-_RELEASE = False
-
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
-
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
+_RELEASE = True
 
 if not _RELEASE:
     _component_func = components.declare_component(
-        # We give the component a simple, descriptive name ("streamlit_arborist"
-        # does not fit this bill, so please choose something better for your
-        # own component :)
         "streamlit_arborist",
-        # Pass `url` here to tell Streamlit that the component will be served
-        # by the local dev server that you run via `npm run start`.
-        # (This is useful while your component is in development.)
         url="http://localhost:3001",
     )
 else:
-    # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to the component's
-    # build directory:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
     _component_func = components.declare_component("streamlit_arborist", path=build_dir)
 
 
+# See `TreeProps` type in the react-arborist package:
 # https://github.com/brimdata/react-arborist/blob/v3.4.0/packages/react-arborist/src/types/tree-props.ts
 def tree_view(
     data: List[dict],
@@ -67,9 +42,7 @@ def tree_view(
     search_term: str = None,
     # Streamlit
     key: Union[str, int] = None,
-    on_change: WidgetCallback = None,
-    args: WidgetArgs = None,
-    kwargs: WidgetKwargs = None,
+    on_change: Callable[..., None] = None,
 ) -> List[dict]:
     """
     Create a tree view.
@@ -78,8 +51,8 @@ def tree_view(
     ----------
     data : List[dict]
         A list of dictionaries representing the tree data.
-        Each dictionary should have an `id` and `name` keys, and may have a `children`
-        key containing a list of child nodes.
+        Each dictionary must have an `id` key.
+        Optional keys are `name` for display (otherwise uses `id`), and `children` for child nodes.
 
     icons : dict, optional
         A dict of keys ``"open"``, ``"closed"``, and ``"leaf"`` with string values
@@ -87,10 +60,7 @@ def tree_view(
         and leaf nodes, respectively.
 
     row_height : int, default 24
-
-    overscan_count : int, default 1
-        Number of additional rows rendered outside the visible viewport to ensure smooth
-        scrolling and better performance.
+        Height of each row in pixels.
 
     width : int or str, default "auto"
         View width in pixels or a valid
@@ -101,6 +71,10 @@ def tree_view(
 
     indent : int, optional
         Node indendation in pixels, by default 24
+
+    overscan_count : int, default 1
+        Number of additional rows rendered outside the visible viewport to ensure smooth
+        scrolling and better performance.
 
     padding_top : int, optional
         Space between the tree and its top border, in pixels.
@@ -121,11 +95,11 @@ def tree_view(
     open_by_default : bool, default True
         Whether all nodes should be open when rendered.
 
-    selection : str or int, optional
-        The node `id` to select and scroll when rendered.
-
     disable_multi_selection : bool, default False
         Whether to disable multi-selection of nodes.
+
+    selection : str or int, optional
+        The node `id` to select and scroll when rendered.
 
     initial_open_state : Dict[str, bool], optional
         A dict of node ID keys and bool values indicating whether the node is open
@@ -142,30 +116,68 @@ def tree_view(
         Multiple widgets of the same type may not share the same key.
 
     on_change : callable, optional
-        An optional callback invoked when the widget's value changes. No arguments are passed to it.
-
-    args : tuple, optional
-        A tuple of arguments to pass to the `on_change` callback.
-
-    kwargs : dict, optional
-        An optional dict of kwargs to pass to the `on_change` callback.
+        An optional callback invoked when a node is selected.
+        No arguments are passed to it.
 
     Returns
     -------
     list of dict
         A list containing the data of selected nodes.
+
+    Examples
+    --------
+    The data should be a list of dictionaries, where each dictionary represents a node
+    in the tree.
+    Each node should have an `id` and `name` keys, and may have a `children` key
+    containing a list of child nodes.
+
+    >>> data = [
+    ...     {
+    ...         "id": "1",
+    ...         "name": "Parent 1",
+    ...         "children": [
+    ...             {"id": "1.1", "name": "Child 1"},
+    ...             {"id": "1.2", "name": "Child 2"}
+    ...         ]
+    ...     },
+    ...     {
+    ...         "id": "2",
+    ...         "name": "Parent 2",
+    ...         "children": [
+    ...             {"id": "2.1", "name": "Child 3"},
+    ...             {"id": "2.2", "name": "Child 4"}
+    ...         ]
+    ...     }
+    ... ]
+
+    >>> from streamlit_arborist import tree_view
+    >>> tree_view(data)
+
+    You can change the key names using `children_accessor` and `id_accessor`.
+
+    >>> data2 = [{"key": "A", "contents": [{"key": "A.1"}, {"key": "A.2"}]}]
+    >>> tree_view(data2, children_accessor="contents", id_accessor="key")
+
+    Customize the appearance of the tree view:
+
+    >>> tree_view(
+    ...     data,
+    ...     icons={"open": "🔓", "closed": "🔒", "leaf": "🍀"},
+    ...     row_height=30,
+    ...     height=400,
+    ...     padding_top=10,
+    ... )
+
+    Add a search term to filter nodes:
+
+    >>> tree_view(data, search_term="Child")
     """
     icons = icons or {}
     icons = {
-        "open": icons.get("open", "📂"),
-        "closed": icons.get("closed", "📁"),
-        "leaf": icons.get("leaf", "📄"),
+        "open": icons.get("open") or "📂",
+        "closed": icons.get("closed") or "📁",
+        "leaf": icons.get("leaf") or "📄",
     }
-
-    args = args or ()
-    kwargs = kwargs or {}
-    if on_change:
-        on_change = partial(on_change, *args, **kwargs)
 
     component_value = _component_func(
         data=data,
@@ -193,7 +205,7 @@ def tree_view(
         # Streamlit
         key=key,
         on_change=on_change,
-        default={},
+        default=[],
     )
 
     return component_value
